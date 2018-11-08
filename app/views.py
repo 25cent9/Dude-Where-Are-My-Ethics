dca273e502300bf303162c9193a1e1fe39b3fc3e
Innocent Niyibizi <25cent9@gmail.com>
2018-11-08 14:44:04 -0600

    Phase II
    First draft of UI as well as a few other housekeeping utilities and views.
    Started the implentation of a state machine that will resemble a moral compass rather than a health tracker.

--- app/views.py
+++ app/views.py
@@ -1,5 +1,27 @@
 from app import app
+from flask import render_template, request, session, redirect
 
-@app.route('/')
-def index():
-    return "Dude, Where Are My Ethics?"
+from random import choice
+
+app.secret_key = 'dude-where-are-my-ethics'
+
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+      session['username'] = request.form['username']
+      return redirect('/game')
+    return render_template('login.html')
+
+    
+@app.route('/game')
+def game():
+    moral_states = ['MG', 'N', "MB"]
+    print(session)
+    if 'state' in session:
+        print("state is good")
+    else:
+        session['state'] = choice(moral_states)
+    print(session)
+    return render_template('game.html', my_string=session['username'],
+            my_list=['The', 'quick', 'brown', 'fox'], moral_state=session['state'])
