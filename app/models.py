a4da423cbf8735d5d635263cca8f5b74a49a8cff
Innocent Niyibizi <25cent9@gmail.com>
2018-11-10 21:57:47 -0600

    Adding the simple state machine

---
+++ app/models.py
@@ -0,0 +1,20 @@
+class Player(object):
+
+    MORALLY_GOOD = "MG"
+    NEUTRAL = "N"
+    MORALLY_BAD = "MB"
+
+    def __init__(self, username, moral_state = "MG"):
+        self.username = username
+        self.moral_state = moral_state
+    
+    def ChangeState(self, moral_value):
+        if(moral_value >= 75):
+            self.moral_state = self.MORALLY_GOOD
+        elif(moral_value == 50):
+            self.moral_state = self.NEUTRAL
+        else:
+            self.moral_state = self.MORALLY_BAD
+
+            
+    
--- app/views.py
+++ app/views.py
@@ -1,5 +1,7 @@
 from app import app
-from flask import render_template, request, session, redirect
+from flask import render_template, request, session, redirect, jsonify
+from app.models import Player
+from json import dumps
 
 from random import choice
 
@@ -9,19 +11,18 @@ app.secret_key = 'dude-where-are-my-ethics'
 @app.route('/', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-      session['username'] = request.form['username']
-      return redirect('/game')
+        new_player = Player(request.form['username'])
+        encoded = dumps(new_player)
+        print(encoded)
+        session['username'] = jsonify(new_player)
+        return redirect('/game')
     return render_template('login.html')
 
     
 @app.route('/game')
 def game():
     moral_states = ['MG', 'N', "MB"]
+    moral_state_values = [100, 75, 50, 25]
     print(session)
-    if 'state' in session:
-        print("state is good")
-    else:
-        session['state'] = choice(moral_states)
-    print(session)
-    return render_template('game.html', my_string=session['username'],
-            my_list=['The', 'quick', 'brown', 'fox'], moral_state=session['state'])
+    return render_template('game.html', my_string="session['username']",
+            my_list=['The', 'quick', 'brown', 'fox'], moral_state=choice(moral_states))
