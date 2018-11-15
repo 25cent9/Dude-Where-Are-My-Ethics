211553a01c916e1c68c658a346cdf96cdee5f470
Innocent Niyibizi <25cent9@gmail.com>
2018-11-15 02:56:47 -0600

    Progression:
    Looks like I can finally send data to and from the webpage. It's really concoluted and ugly but if it fits, it ships.
    Need to work on the small bug for first month.

--- app/utils.py
+++ app/utils.py
@@ -1,3 +1,8 @@
+HUNNIT = "100.png"
+SEVENTY = "75.png"
+FITY = "50.png"
+TWENTY_FIVE = "25.png"
+
 def read_prompts(prompt_number):
     prompt_text_file = open("app/static/prompts/prompt"+str(prompt_number)+"/text.txt")
     prompt_text_list = []
@@ -10,3 +15,13 @@ def gather_images(prompt_number):
     root_folder = "../static/prompts/prompt"+str(prompt_number)+"/"
     images = [root_folder+"100.png", root_folder+"75.png", root_folder+"50.png", root_folder+"25.png"]
     return images
+
+def parse_image_location(image_locatin):
+    values = [HUNNIT, SEVENTY, FITY, TWENTY_FIVE]
+    for value in values:
+        index = image_locatin.find(value)
+        if index > 0:
+            return value[0:len(value)-4]
+
+    
+    
--- app/views.py
+++ app/views.py
@@ -2,11 +2,12 @@ from app import app
 from flask import render_template, request, session, redirect, jsonify
 from app.models import Player
 from json import dumps
-from app.utils import read_prompts, gather_images
+from app.utils import read_prompts, gather_images, parse_image_location
 from random import choice, randint
 
 app.secret_key = 'dude-where-are-my-ethics'
-
+MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 
+            'July', 'August', 'September', 'October', 'November', 'December']
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
@@ -15,23 +16,31 @@ def login():
         new_player = Player(request.form['username'], moral_state=choice(moral_states))
         session['username'] = new_player.username
         session['moral_state'] = new_player.moral_state
+        session['prompt_number'] = 1
         return redirect('/game')
     return render_template('login.html')
 
     
 @app.route('/game', methods=['GET', 'POST'])
 def game():
-    months = [
-            'January', 'February', 'March', 'April', 'May', 'June', 
-            'July', 'August', 'September', 'October', 'November', 'December']
-    if request.method == 'POST':
-        print("Button has been pressed")
-        return render_template('game.html', my_string=session['username'],
-            my_list=['The', 'quick', 'brown', 'fox'], moral_state=session["moral_state"])
-    current_month = randint(1,15)
+    print(session['prompt_number'])
+    current_month = session['prompt_number']
     prompts = read_prompts(current_month)
     images = gather_images(current_month)
-    print(images)
     actions = prompts[1:]
-    return render_template('game.html', month=months[current_month%len(months)-1], 
-        moral_state=session["moral_state"], current_prompt=prompts[0], actions=actions, prompt_images=images)
+    if request.method == 'POST':
+        elements = []
+        for value in request.form:
+            elements.append(value)
+        response = parse_image_location(elements[0])
+        if session["prompt_number"] == 15:
+            session["prompt_number"] = 1
+        elif session["prompt_number"] >= 1:
+            session['prompt_number'] += 1
+        current_month = session['prompt_number']
+        prompts = read_prompts(current_month)
+        images = gather_images(current_month)
+        return render_template('game.html', month=MONTHS[current_month%len(MONTHS)], 
+            prompt_number=session["prompt_number"], current_prompt=prompts[0], actions=actions, prompt_images=images)
+    return render_template('game.html', month=MONTHS[0], 
+        prompt_number=session["prompt_number"], current_prompt=prompts[0], actions=actions, prompt_images=images)
